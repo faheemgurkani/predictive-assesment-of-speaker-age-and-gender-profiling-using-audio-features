2e02025ad179dd7eca0eff2f28a626817c092614
FaheemGurkani <faheemGurkani@gmail.com>
2024-09-05 09:27:44 +0500

    Notebook Update

--- data/Dataset/README.md
+++ data/Dataset/README.md
@@ -1,3 +1,5 @@
 ### The link for the respective dataset is as follows: https://kaggle.com/datasets/8ea8c07fe5877b94e3d434be13c4b04f8b4585643dcebaae972f9ddedf401c1f
 
-**Note:** Due to GitHub's restriction on uploading files of size greater than 100 MB; I have provided the link to the relevant dataset (published on my `kaggle` account).
+**Note:** 
+- Due to GitHub's restriction on uploading files of size greater than 100 MB; I have provided the link to the relevant dataset (published on my `kaggle` account).
+- Use version 2 of the dataset with the updated version of the notebook
--- src/model.py
+++ src/model.py
@@ -1,6 +1,42 @@
 import numpy as np
+import pandas as pd
 
-def manual_linear_regression(x, y):
+
+
+def ridge_based_brute_linear_regression(x, y, lambda_reg=1e-5):
+    # Adding a column of ones for bias
+    x_with_bias = np.c_[x, np.ones(x.shape[0])]
+
+    # Calculating X transpose
+    x_transpose = x_with_bias.T
+
+    # Calculating X transpose multiplied by X
+    x_transpose_x = np.dot(x_transpose, x_with_bias)
+
+    # Adding regularization term (lambda_reg * I)
+    regularization_term = lambda_reg * np.eye(x_transpose_x.shape[0])
+    x_transpose_x_reg = x_transpose_x + regularization_term
+
+    # Calculating the inverse of X_transpose_X with regularization
+    x_transpose_x_inv = np.linalg.inv(x_transpose_x_reg)
+
+    # Calculating (X_transpose_X)^-1 multiplied by X transpose
+    a = np.dot(x_transpose_x_inv, x_transpose)
+
+    # Converting y to a NumPy array and reshaping it
+    y_array = y.to_numpy().reshape(-1, 1)
+
+    # Calculating the final parameters
+    final = np.dot(a, y_array)
+
+    # Extracting weights and bias
+    weights = final[:-1]
+    bias = final[-1]
+
+    return final, weights, bias
+
+# Implementation based function
+def collinearity_based_brute_linear_regression(x, y):
     # Adding a column of ones for bias
     x_with_bias = np.c_[x, np.ones(x.shape[0])]
 
@@ -16,8 +52,11 @@ def manual_linear_regression(x, y):
     # Calculating (X_transpose_X)^-1 multiplied by X transpose
     a = np.dot(x_transpose_x_inv, x_transpose)
 
+    # Converting y to a NumPy array and reshaping it
+    y_array = y.to_numpy().reshape(-1, 1)
+
     # Calculating the final parameters
-    final = np.dot(a, y.reshape(-1, 1))
+    final = np.dot(a, y_array)
 
     # Extracting weights and bias
     weights = final[:-1]
